a9439475ec740892d0006f8383f92644f965c0a0
Victor Aro <victor.galves@live.com>
2021-08-26 02:25:28 -0300

    .

--- components/lights/lights.js
+++ components/lights/lights.js
@@ -36,7 +36,7 @@ Vue.component('lights', {
       document.documentElement.style.setProperty('--primary', '#f0f0f0');
       document.documentElement.style.setProperty('--secondary', '#303030');
       document.documentElement.style.setProperty('--background', '#f0f0f0');
-      document.documentElement.style.setProperty('--navbar', '#d4d4d4');
+      document.documentElement.style.setProperty('--navbar', '#f5f5f5');
       document.getElementById("line-pattern").style.filter = "invert(100)"
     }
   },
